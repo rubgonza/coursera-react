fbb8e155c7f67d95547c78181e571d5cde63516f
rubgonza <tsystemsrubgonza@gmail.com>
2022-05-10 17:40:42 +0200

    Single Page Applications Part 2

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 //Functional component no esta en una clase ni extiende de component!
 function RenderDish({ dish }) {
@@ -18,9 +27,9 @@ function RenderDish({ dish }) {
   }
 }
 
-function RenderComments({ dish }) {
-  if (dish != null) {
-    const comm = dish.comments.map((comment) => {
+function RenderComments({ comments }) {
+  if (comments != null) {
+    const comm = comments.map((comment) => {
       return (
         <li key={comment.id}>
           <p>{comment.comment}</p>
@@ -50,11 +59,23 @@ const DishDetail = (props) => {
   return (
     <div className="container">
       <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/menu">Menu</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>{props.dish.name}</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
         <div className="col-12 col-md-5 m-1">
           <RenderDish dish={props.dish} />
         </div>
         <div className="col-12 col-md-5 m-1">
-          <RenderComments dish={props.dish} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     </div>
