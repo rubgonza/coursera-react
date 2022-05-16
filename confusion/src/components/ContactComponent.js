6e27a30b38e705ea6fe16cc01d9e1bdb438ec183
rubgonza <tsystemsrubgonza@gmail.com>
2022-05-16 17:40:07 +0200

    React Redux Forms Revisited

--- confusion/src/components/ContactComponent.js
+++ confusion/src/components/ContactComponent.js
@@ -10,7 +10,7 @@ import {
   Label,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Control, LocalForm, Errors } from "react-redux-form";
+import { Control, Form, Errors } from "react-redux-form";
 
 class Contact extends Component {
   constructor(props) {
@@ -52,10 +52,11 @@ class Contact extends Component {
     });
   }
 
-  handleSubmit(event) {
-    console.log("Current State is: " + JSON.stringify(this.state));
-    alert("Current State is: " + JSON.stringify(this.state));
-    event.preventDefault();
+  handleSubmit(values) {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
+    // event.preventDefault();
   }
 
   render() {
@@ -131,7 +132,10 @@ class Contact extends Component {
             <h3>Send us your Feedback</h3>
           </div>
           <div className="col-12 col-md-9">
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+            <Form
+              model="feedback"
+              onSubmit={(values) => this.handleSubmit(values)}
+            >
               <Row className="form-group">
                 <Label htmlFor="firstname" md={2}>
                   First Name
@@ -293,7 +297,7 @@ class Contact extends Component {
                   </Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
